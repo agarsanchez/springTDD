6ae349127ac75f2dff860b75a6813bb5cd18ff6a
Alberto Garcia <albertogarcia1984@gmail.com>
2017-06-13 22:23:17 +0100

    Setup project

---
+++ src/main/java/com/agarsanchez/springtdd/SpringtddApplication.java
@@ -0,0 +1,12 @@
+package com.agarsanchez.springtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtddApplication.class, args);
+	}
+}
